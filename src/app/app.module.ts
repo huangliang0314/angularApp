e536fb7d9aa732ed1a1f8486a98b6eca4d09e6f4
huangliang0314 <909589592@qq.com>
2018-05-09 17:57:58 +0800

    223

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroService } from './hero.service';
+import { MessageComponent } from './message/message.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessageComponent
   ],
   imports: [
     BrowserModule,
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,13 +1,21 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './Entity/hero';
 import { Heroes } from './Entity/mock-heroes';
-import { Observable } from 'rxjs/observable';
-import { of } from 'rxjs/Observable/of';
+
+/* 
+        下面导入的另一种写法目前报错
+        import { Observable，of } from 'rxjs';/
+*/
+import { Observable } from 'rxjs/Observable';/* Observable注意大小写 */
+import { of } from 'rxjs/observable/of';/* Observable注意大小写 */
+
+
 @Injectable()
 export class HeroService {
 
   constructor() { }
   getHeroes(): Observable<Hero[]> {
+    // 
     return of(Heroes);
   }
 }
